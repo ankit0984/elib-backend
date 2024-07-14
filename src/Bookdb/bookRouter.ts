b7f2767c07725dbcdcdd3c273fc8a7a6d6258d79
akki <ak1410.com>
2024-07-15 01:15:08 +0530

    modify tsconfig

--- src/Bookdb/bookRouter.ts
+++ src/Bookdb/bookRouter.ts
@@ -1,25 +1,3 @@
-// import Router  from "express";
-// import { updateBook } from "./updateDetails/updateBookController";
-// import {createBook} from "./bookUpload/bookDBController"
-// import upload from "../middleware/fileUpload";
-// import { listBooks } from "./getBooks/bookDetailsController";
-// // import {bookDB} from "./Login/loginController";
-
-// const bookRouter = Router();
-
-// bookRouter.post('/bookdb/add',upload.fields([
-//     {name:'coverImage',maxCount:1},
-//     {name:'file',maxCount:1}
-// ]), createBook)
-
-// bookRouter.put(
-    
-//     '/bookdb/update:bookId',upload.fields([
-//     {name:'coverImage',maxCount:1},
-//     {name:'file',maxCount:1}
-// ]), updateBook)
-// bookRouter.get('/bookdb', listBooks)
-// bookRouter.get('/bookdb:bookId', listBooks)
 
 
 
